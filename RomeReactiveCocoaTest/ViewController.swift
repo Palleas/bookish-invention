bd4b2b9211810c24a818daf0c2905f07e5373ee3
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-03 16:26:31 -0400

    Add stupid use of ReactiveCocoa

--- RomeReactiveCocoaTest/ViewController.swift
+++ RomeReactiveCocoaTest/ViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import ReactiveSwift
+import ReactiveCocoa
+import Result
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolSwitch: UISwitch!
+    @IBOutlet weak var otherSwitch: UISwitch!
+    @IBOutlet weak var yetAnotherSwitch: UISwitch!
+
+    let coolAction = Action<Bool, Bool, NoError> { input in
+        return SignalProducer(value: input)
+    }
+
+    var switchAction: CocoaAction<UISwitch>!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        switchAction = CocoaAction<UISwitch>(coolAction, { s in
+            return s.isOn
+        })
+        
+        coolSwitch.addTarget(switchAction, action: CocoaAction<UISwitch>.selector, for: .valueChanged)
+        
+        otherSwitch.reactive.isOn <~ coolSwitch.reactive.isOnValues
+        yetAnotherSwitch.reactive.isOn <~ coolSwitch.reactive.isOnValues.map { !$0 }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doSomethingCool(_ sender: Any) {
+        
     }
 
 
